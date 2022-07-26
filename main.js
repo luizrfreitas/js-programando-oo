c73457b7152f76211ef5f8f42a6dd3a66f1bb19f
Luiz R Freitas <luizfreitasaca@gmail.com>
2022-07-26 10:44:53 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const cliente1Nome = "Ricardo";
+const cliente1CPF = 11122233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2CPF = 44422233309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
+
+class Cliente {
+
+}
